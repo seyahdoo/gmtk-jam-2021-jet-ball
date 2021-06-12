40a5a508eb50fb16df56015b9cc569f819ece808
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2021-06-12 10:05:36 +0300

    fix inputs

--- Assets/Character/Character.cs
+++ Assets/Character/Character.cs
@@ -7,20 +7,32 @@ using UnityEngine;
 public class Character : MonoBehaviour {
     public bool isLeft = false;
     public Vector2 input = Vector2.zero;
+    public bool stickButtonPressed = false;
+    public InputSettings inputSettings;
+    
     public float maxFuel = 10f;
     public float inputToForceRatio = 1f;
     private float _currentFuel;
     private Rigidbody2D _rigidbody;
+    private void OnEnable() {
+        inputSettings.Enable();
+    }
+    private void OnDisable() {
+        inputSettings.Disable();
+    }
     private void Awake() {
+        inputSettings = new InputSettings();
         _rigidbody = GetComponent<Rigidbody2D>();
         _currentFuel = maxFuel;
     }
     private void Update() {
         if (isLeft) {
-            input = new Vector2(Input.GetAxis("HorizontalLeft"), Input.GetAxis("VerticalLeft"));
+            input = inputSettings.Character.LeftMovement.ReadValue<Vector2>();
+            stickButtonPressed = Math.Abs(inputSettings.Character.LeftStick.ReadValue<float>() - 1) < .1f;
         }
         else {
-            input = new Vector2(Input.GetAxis("HorizontalRight"), Input.GetAxis("VerticalRight"));
+            input = inputSettings.Character.RightMovement.ReadValue<Vector2>();
+            stickButtonPressed = Math.Abs(inputSettings.Character.RightStick.ReadValue<float>() - 1) < .1f;
         }
     }
     private void FixedUpdate() {
