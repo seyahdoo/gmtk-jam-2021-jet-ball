15888b8ea60b9e2c25bd038912dd6693f8b63928
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2021-06-12 16:59:39 +0300

    refactor

--- Assets/Character/Ball.cs
+++ Assets/Character/Ball.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Ball : MonoBehaviour {
